be7544a372775973ecd2405267374b9681d5e5e1
UDilakshan <uthayakumardilakshan@gmail.com>
2025-03-22 06:54:48 +0530

    cloning

--- src/actions.tsx
+++ src/actions.tsx
@@ -3,7 +3,7 @@
 import  ImageKit  from "imagekit";
 
 const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
   privateKey: process.env.PRIVATE_KEY!,
   urlEndpoint : process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import ImageKit from "imagekit"
 
 export const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
   privateKey: process.env.PRIVATE_KEY!,
   urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
 });
