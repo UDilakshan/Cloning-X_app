0a848886e5a3e85ca93e3ad5fa3d1ac440584dda
UDilakshan <uthayakumardilakshan@gmail.com>
2025-03-22 11:59:11 +0530

    Adjusted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,33 +2,35 @@ import LeftBar from "@/components/LeftBar";
 import "./globals.css";
 import RightBar from "@/components/RightBar";
 
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Lama Dev X Clone',
+  description: 'Next.js social media application project',
+}
+
 export default function RootLayout({
   children,
+  modal
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* ✅ Added viewport meta tag to fix zoom issue on mobile */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body className="text- leading-normal">
-      <div className="container mx-auto px-4">
-      <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto flex justify-between pt-5">
-      
-      <div className="px-1 xsm:px-4 xxl:px-8 ">
-          <LeftBar />
+      <body>
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
+          <div className="px-1 xsm:px-4 xxl:px-8 ">
+            <LeftBar />
+          </div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
+            {children}
+            {modal}
+          </div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
+            <RightBar />
+          </div>
         </div>
-        <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
-          {children}
-        </div>
-        <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
-          <RightBar />
-        </div>
-      </div>
-      </div>
-
       </body>
     </html>
   );
