9811a52882735a9be53e46d18e859160d3a850ef
UDilakshan <uthayakumardilakshan@gmail.com>
2025-03-22 11:21:00 +0530

    Zooming function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,17 +9,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Added viewport meta tag to fix zoom issue on mobile */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </head>
       <body>
-      <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between pt-5">
-      <div className="px-2 xsm:px-4 xxl:px-8 ">
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between pt-5">
+          <div className="px-2 xsm:px-4 xxl:px-8 ">
             <LeftBar />
-      </div>
-      <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
+          </div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
             {children}
-      </div>
-      <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
+          </div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
             <RightBar />
-      </div>
+          </div>
         </div>
       </body>
     </html>
