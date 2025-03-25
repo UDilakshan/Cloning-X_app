0556a78d0d17543a9fb58acbaa09d26b3d8d3915
UDilakshan <uthayakumardilakshan@gmail.com>
2025-03-25 13:57:24 +0530

    Latest updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
  import Feed from "@/components/Feed";
  import Share from "@/components/Share";
+import { prisma } from "@/prisma";
  import Link from "next/link";
 
 const Homepage = () => {
+
   return (
   <div className="">
     <div className='px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
